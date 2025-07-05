e02e230b502eafba71e0dfdbe3f60982a43094e6
JinhengZ <jxz230031@utdallas.edu>
2025-07-05 16:42:07 -0500

    doc(assets.js): add 8 pic into product, product desciption need to update

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -9,6 +9,21 @@ import exchange_icon from './exchange_icon.png'
 import quality_icon from './quality_icon.png'
 import support_icon from './support_icon.png'
 import cross_icon from './cross_icon.png'
+import star_icon from './star_icon.png'
+import star_half_icon from './half_star_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+
+/*----------------- import cloth pic------------------*/
+import cloth_001 from './cloth_001.png'
+import cloth_002 from './cloth_002.png'
+import cloth_003 from './cloth_003.png'
+import cloth_004 from './cloth_004.png'
+import cloth_005 from './cloth_005.png'
+import cloth_006 from './cloth_006.png'
+import cloth_007 from './cloth_007.png'
+import cloth_008 from './cloth_008.png'
+import cloth_009 from './cloth_009.png'
+import cloth_010 from './cloth_010.png'
 
 
 export const assets = {
@@ -22,32 +37,147 @@ export const assets = {
   exchange_icon,
   quality_icon,
   support_icon,
-  cross_icon
+  cross_icon,
+  star_icon,
+  star_dull_icon,
+  star_half_icon,
+  cloth_001,
+  cloth_002,
+  cloth_003,
+  cloth_004,
+  cloth_005,
+  cloth_006,
+  cloth_007,
+  cloth_008,
+  cloth_009,
+  cloth_010,
 }
 
 export const products = [
     {
         _id: "aaaaa",
-        name: "Women Round Nec Cotten Top",
+        name: "Men Round Nec Cotten Top",
         description: "A lighweight, usually one",
         price: 100,
-        image: [junimo],
-        category : "Women",
+        image: [cloth_001],
+        category : "Men",
         subCategory: "Topwear",
         sizes: ["S", "M", "L"],
         date: 1716634345448,
         bestseller: true
     },
-        {
+    {
         _id: "aaaab",
         name: "Men V Nec Fit Top",
         description: "A lighweight, usually one",
         price: 200,
-        image: [logo,search_icon],
+        image: [cloth_002,junimo],
+        category : "Men",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaac",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_003],
+        category : "Men",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaad",
+        name: "Women V Nec Fit Dress",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_004],
+        category : "Women",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaae",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 500,
+        image: [cloth_005],
+        category : "Women",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    }
+    ,
+    {
+        _id: "aaaaf",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 700,
+        image: [cloth_006],
+        category : "Women",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    }
+    ,
+    {
+        _id: "aaaag",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_007],
         category : "Men",
         subCategory: "Topwear",
         sizes: ["M", "L", "XL"],
         date: 1716634345448,
         bestseller: false
     }
+    ,
+    {
+        _id: "aaaah",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_008],
+        category : "Men",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    }
+    ,
+    {
+        _id: "aaaai",
+        name: "Men V Nec Fit Top",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_009],
+        category : "Women",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    }
+    ,
+    {
+        _id: "aaaag",
+        name: "Kid cute cutton Top",
+        description: "A lighweight, usually one",
+        price: 200,
+        image: [cloth_010],
+        category : "Kids",
+        subCategory: "Topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716634345448,
+        bestseller: false
+    }
+    
 ]

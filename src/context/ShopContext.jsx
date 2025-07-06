45a750ec3d01171c3d66fbeac58fb3fcd3754f1c
JinhengZ <jxz230031@utdallas.edu>
2025-07-06 11:15:15 -0500

    feat(Cart, CartTotal): add check out button and caculate total amount function, TODO: Add Place Order Page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -64,11 +64,29 @@ const ShopContextProvider = (props) => {
     }
 
 
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((products)=> products._id === items);
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                }catch(error){
+
+                }
+            }
+        }
+        return totalAmount;
+    }
+
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
-        getCartCount, updateQuantity
+        getCartCount, updateQuantity,
+        getCartAmount
     }
 
     return(
